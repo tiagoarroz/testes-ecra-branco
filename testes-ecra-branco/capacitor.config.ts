bf46d98b9d2d04c431d9aad4cb937bdbc010f399
tiagoarroz <arrozinhoquentinho@hotmail.com>
2025-09-19 15:51:31 +0100

    tentativa de ativar debugging da webview

--- testes-ecra-branco/capacitor.config.ts
+++ testes-ecra-branco/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'testes-ecra-branco',
-  webDir: 'www'
+  webDir: 'www',
+  android: 
+  {
+    webContentsDebuggingEnabled: true
+  }
 };
 
 export default config;
